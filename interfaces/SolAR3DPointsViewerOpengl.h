9615b1e23d43c05810fb742e8625f4f0f05de73c
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2021-01-20 17:13:40 +0100

    doc: add component properties documentation

--- interfaces/SolAR3DPointsViewerOpengl.h
+++ interfaces/SolAR3DPointsViewerOpengl.h
@@ -40,6 +40,71 @@ namespace OPENGL {
  * Configuration parameters allow user to visualize the axis of the coordinate systems of the world, the center of the point cloud, and the camera.
  * The color of points can be fixed, or can be the one assigned to each point.
  * The scale of the points, camera and coordinate systems axis can be defined by the usr thanks to configuration parameters.
+ *
+ * @SolARComponentPropertiesBegin
+ * @SolARComponentProperty{ title,
+ *                          the title of the window on which the image will be displayed,
+ *                          @SolARComponentPropertyDescString{ "" }}
+ * @SolARComponentProperty{ width,
+ *                          the width of the window on which the image will be displayed (if <=0\, the width of the input image),
+ *                          @SolARComponentPropertyDescNum{ int, [0..MAX INT], 0 }}
+ * @SolARComponentProperty{ height,
+ *                          the height of the window on which the image will be displayed (if <=0\, the height of the input image),
+ *                          @SolARComponentPropertyDescNum{ int, [0..MAX INT], 0 }}
+ * @SolARComponentProperty{ backgroundColor,
+ *                          background color,
+ *                          @SolARComponentPropertyDescList{ 3, uint, [0..255], { 255\, 255\, 255 } }}
+ * @SolARComponentProperty{ fixedPointsColor,
+ *                          if 0, the color of each point is used\, else the color defined in parameter by user is used,
+ *                          @SolARComponentPropertyDescNum{ uint, [0,1], 1 }}
+ * @SolARComponentProperty{ pointsColor,
+ *                          points color,
+ *                          @SolARComponentPropertyDescList{ 3, uint, [0..255], { 0\, 255\, 0 } }}
+ * @SolARComponentProperty{ points2Color,
+ *                          points color for the second cloud,
+ *                          @SolARComponentPropertyDescList{ 3, uint, [0..255], { 255\, 0\, 0 } }}
+ * @SolARComponentProperty{ cameraColor,
+ *                          camera color,
+ *                          @SolARComponentPropertyDescList{ 3, uint, [0..255], { 0\, 0\, 255 } }}
+ * @SolARComponentProperty{ keyframeAsCamera,
+ *                          if not 0\, each keyframe pose is drawn as a camera\, else as a point,
+ *                          @SolARComponentPropertyDescNum{ uint, [0\,1], 0 }}
+ * @SolARComponentProperty{ framesColor,
+ *                          frame color,
+ *                          @SolARComponentPropertyDescList{ 3, uint, [0..255], { 180\, 180\, 180 } }}
+ * @SolARComponentProperty{ keyframesColor,
+ *                          keyframe color,
+ *                          @SolARComponentPropertyDescList{ 3, uint, [0..255], { 0\, 255\, 0 } }}
+ * @SolARComponentProperty{ keyframes2Color,
+ *                          keyframe color for the second vector of keyframe,
+ *                          @SolARComponentPropertyDescList{ 3, uint, [0..255], { 255\, 0\, 0 } }}
+ * @SolARComponentProperty{ drawCameraAxis,
+ *                          if not 0\, a gizmo showing the coordinate system of the camera is displayed,
+ *                          @SolARComponentPropertyDescNum{ uint, [0\,1], 1 }}
+ * @SolARComponentProperty{ drawSceneAxis,
+ *                          if not 0\, a gizmo showing the coordinate system axis of the scene reference is displayed,
+ *                          @SolARComponentPropertyDescNum{ uint, [0\,1], 1 }}
+ * @SolARComponentProperty{ drawWorldAxis,
+ *                          if not 0\, a gizmo showing the coordinate system axis of the world reference is displayed,
+ *                          @SolARComponentPropertyDescNum{ uint, [0\,1], 1 }}
+ * @SolARComponentProperty{ axisScale,
+ *                          define the scale of the gizmo displaying the coordinate system center on the scene,
+ *                          @SolARComponentPropertyDescNum{ float, [0..MAX FLOAT], 1.f }}
+ * @SolARComponentProperty{ pointSize,
+ *                          size of points,
+ *                          @SolARComponentPropertyDescNum{ float, [0..MAX FLOAT], 2.f }}
+ * @SolARComponentProperty{ cameraScale,
+ *                          camera scale,
+ *                          @SolARComponentPropertyDescNum{ float, [0..MAX FLOAT], 1.f }}
+ * @SolARComponentProperty{ zoomSensitivity,
+ *                          zoom sensitivity,
+ *                          @SolARComponentPropertyDescNum{ float, [0..MAX FLOAT], 10.f }}
+ * @SolARComponentProperty{ exitKey,
+ *                          the key code to press to close the window. If negative\, no key is defined to close the window,
+ *                          @SolARComponentPropertyDescNum{ int, [-1..MAX INT], 27 }}
+ *
+ * @SolARComponentPropertiesEnd
+ *
  */
 
 class SOLAROPENGL_EXPORT_API SolAR3DPointsViewerOpengl : public org::bcom::xpcf::ConfigurableBase,
